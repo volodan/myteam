9bd04b4032ba548aa02a61e8b6c8dda976771d42
Vladislav Jevcak <P3501100@ness.com>
2017-09-21 16:50:31 +0200

    dto for count positions

---
+++ myteam/src/main/java/com/ness/myteam/dto/RoleNumberPositionsDTO.java
@@ -0,0 +1,51 @@
+package com.ness.myteam.dto;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * DTO contains data for counting numbers for positions
+ */
+public class RoleNumberPositionsDTO {
+
+    private String roleName;
+    private Integer counter;
+ 
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Integer getCouner() {
+        return counter;
+    }
+
+    public void setCounter(Integer counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof RoleNumberPositionsDTO) {
+            if (o == null) {
+                return false;
+            } else {
+                RoleNumberPositionsDTO dto = (RoleNumberPositionsDTO)o; 
+                if (!StringUtils.isEmpty(dto.getRoleName()) && !StringUtils.isEmpty(this.roleName) && dto.getRoleName().equals(this.roleName)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return StringUtils.isEmpty(this.roleName) ? super.hashCode() : this.roleName.hashCode();
+    }
+}
